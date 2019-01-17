c6a6f0d1bb4043140245723bf41f3f29222d40f4
fengzongming <956626817@qq.com>
2019-01-17 10:27:29 +0800

    practice files

---
+++ demo02_多任务/demo_01_没有多任务的程序.py
@@ -0,0 +1,20 @@
+import time
+
+
+def sing():
+    for i in range(5):
+        print("唱歌%d" % i)
+
+
+def dance():
+    for i in range(5):
+        print("跳舞%d" % i)
+
+
+def main():
+    sing()
+    dance()
+
+
+if __name__ == '__main__':
+    main()
---
+++ demo02_多任务/demo_04_定义类实现多任务.py
@@ -0,0 +1,14 @@
+import time
+import threading
+
+
+class MyThread(threading.Thread):
+    def run(self):
+        for i in range(3):
+            time.sleep(1)
+            msg = "I am " + self.name + " @ " + str(i)
+            print(msg)
+
+if __name__ == '__main__':
+    t = MyThread()
+    t.start()
